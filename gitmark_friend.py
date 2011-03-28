80f63e1c1868129ffc8f95ecb799599d06cff152
Far McKon <FarMcKon@gmail.com>
2011-03-27 20:47:06 -0400

    added start of friends infrastructure

---
+++ gitmark_friend.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+gitmark_add.py
+
+Functions and classes to grab friends data from their repo 
+
+Based on gitmarks by Hilary Mason on 2010-09-24.
+Copyright 2010 by Far McKon (intermediate while picking a opensource license)
+"""
+
+
+
+if __name__ == "__main__":
+	print "goddammed! Do some friend stuff here"
+	autoadd_friends_tagged_bookmarks();
