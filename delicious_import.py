e78f901f106d7cf79c701a4b37beb5f43a68857b
Far McKon <FarMcKon@gmail.com>
2011-04-17 11:45:52 -0400

    reapplied pep8 changes.

--- delicious_import.py
+++ delicious_import.py
@@ -16,22 +16,25 @@ from gitmark import gitmark
 from gitmark_add import addToRepo, addToPublicRepo
 
 def cache_to_local_file(local_file, content):
-    """Save content in local file"""
-    h = open(local_file, 'w')
-    h.write(content)
-    h.close()
+	"""Save content in local file"""
+	h = open(local_file, 'w')
+	h.write(content)
+	h.close()
 
 
 def import_delicious_to_local_git(username, password='', url=None):
 	""" imports a delicious file to the local git system. If url is not set
-	a delicious API url is generated. if url is set (for a file, for example) that file is imported."""
+	a delicious API url is generated. if url is set (for a file, for example) 
+	that file is imported."""
 	if not url:
 			# API URL: https://user:passwd@api.del.icio.us/v1/posts/all
-		url = "https://%s:%s@api.del.icio.us/v1/posts/all" % (username, password)
+		url = "https://%s:%s@api.del.icio.us/v1/posts/all" % (username, 
+																password)
 		#req = urllib2.Request(url, headers={'Accept':'application/xml'})
 		h = urllib.urlopen(url)
 	else:
-		url = urllub.pathname2url(url)  #url is actually a local file in this case
+		# Url is actually a local file in this case
+		url = urllub.pathname2url(url)	
 		h = open(url)
 	content = h.read()	
 	h.close()
@@ -47,7 +50,6 @@ def import_delicious_to_local_git(username, password='', url=None):
 	try:
 		x = minidom.parseString(content)
 	except expat.ExpatError, e:
-		print content
 		saveFile = "minidom_freakout.xml"
 		fh = open(saveFile, "w")
 		print "== Above content caused minidom to flipped out\n %s" % (e)
@@ -104,59 +106,55 @@ def import_delicious_to_local_git(username, password='', url=None):
 		# FUTURE: speeed this up, by passing a whole list
 		print "adding mark %s to repo %s" %(str(mark.title), str(mark.private) )
 		err = addToRepo(mark,doPush=False)
-		print "mark add error %s" %str(err)	
+		if (err > 0):
+				print "mark add error %s" %str(err) 
 	return 0
 
 # -- hack test main for when yahoo sucks and I need to test
 if __name__ == '__offfline_main__':
-    x = {"extended": "",
-        "hash": "082d479d946d5e9ebd891509446d9cbc",
-        "description":
-                "SSH and SCP: Howto, tips & tricks \u00ab Linux Tutorial Blog",
-        "rights": None,
-        "creator": "delicious:farmckon",
-        "uri": "http://www.linuxtutorialblog.com/post/ssh-and-scp-howto-tips-tricks",
-        "private": False,
-        "meta": "09f8b3205ee44cac3a94305db4337a7b",
-        "time": "2011-02-05T21:16:48Z",
-        "tags": [
-            "ssh",
-            "scp",
-            "linux_tutorial",
-            "howto"]}
+	x = {"extended": "",
+		"hash": "082d479d946d5e9ebd891509446d9cbc",
+		"description": "SSH and SCP: Howto, tips & tricks \u00ab Linux Tutorial Blog",
+		"rights": None,
+		"creator": "delicious:farmckon",
+		"uri": "http://www.linuxtutorialblog.com/post/ssh-and-scp-howto-tips-tricks",
+		"private": False,
+		"meta": "09f8b3205ee44cac3a94305db4337a7b",
+		"time": "2011-02-05T21:16:48Z",
+		"tags": ["ssh", "scp", "linux_tutorial","howto"]}
 
-    g = gitmark(x['uri'], x['creator'])
-    g.description = x['description']
-    g.tags = x['tags']
-    g.time = x['time']
-    g.rights = None
-    g.meta = x["meta"]
-    g.extended = x['extended']
-    g.private = x['private']
-    addToPublicRepo(g)
+	g = gitmark(x['uri'], x['creator'])
+	g.description = x['description']
+	g.tags = x['tags']
+	g.time = x['time']
+	g.rights = None
+	g.meta = x["meta"]
+	g.extended = x['extended']
+	g.private = x['private']
+	addToPublicRepo(g)
 
 
 if __name__ == '__main__':
-    usage = """
-        Usage: python delicious_import.py cached-page-uri
-        OR
-        Usage: python delicious_import.py username password
-        ***Password and username are sent as HTTPS***"
-        """
+	usage = """
+		Usage: python delicious_import.py cached-page-uri
+		OR
+		Usage: python delicious_import.py username password
+		***Password and username are sent as HTTPS***"
+		"""
 
-    if (len(sys.argv) == 2):
-        import getpass
-        import socket
+	if (len(sys.argv) == 2):
+		import getpass
+		import socket
 
-        username = getpass.getuser()
-        host = socket.gethostname()
-        username = '%s@%s' % (str(username), str(host))
-        import_delicious_to_local_git(username, password=None, url=sys.argv[1])
-    elif (len(sys.argv) == 3):
-        try:
-            (username, password) = sys.argv[1:]
-        except ValueError:
-            print usage
-        import_delicious_to_local_git(username, password)
-    else:
-        print usage
+		username = getpass.getuser()
+		host = socket.gethostname()
+		username = '%s@%s' % (str(username), str(host))
+		import_delicious_to_local_git(username, password=None, url=sys.argv[1])
+	elif (len(sys.argv) == 3):
+		try:
+			(username, password) = sys.argv[1:]
+		except ValueError:
+			print usage
+		import_delicious_to_local_git(username, password)
+	else:
+		print usage
